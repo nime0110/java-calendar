56ff63ce77ffd9c3352caece473625df355c9a9e
nime0110 <77383149+nime0110@users.noreply.github.com>
2021-12-11 16:13:32 +0900

    test

--- src/jjk/calendar/Calendar.java
+++ src/jjk/calendar/Calendar.java
@@ -8,5 +8,6 @@ public class Calendar {
 		System.out.println(" 8 9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		System.out.println("22 23 24 25 26 27 28");
 	}
 }
