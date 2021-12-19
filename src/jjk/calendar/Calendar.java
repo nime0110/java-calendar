23cb87de99328b1812b3f30b6ebcd09f3e464dc2
nime0110 <77383149+nime0110@users.noreply.github.com>
2021-12-19 17:20:13 +0900

    캘린더 해답 코드 수정(1)

--- src/jjk/calendar/Calendar.java
+++ src/jjk/calendar/Calendar.java
@@ -1,4 +1,5 @@
 package jjk.calendar;
+import java.util.Scanner;
 
 public class Calendar {
 	public static void main(String[] args) {
@@ -8,5 +9,11 @@ public class Calendar {
 		System.out.println(" 8 9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		
+		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램 
+		Scanner sc = new Scanner(System.in);		
+		System.out.println("달을 입력하세요");
+		int month = sc.nextInt();
+		 
 	}
 }
--- src/jjk/calendar/MaxCalendars.java
+++ src/jjk/calendar/MaxCalendars.java
@@ -5,12 +5,9 @@ public class MaxCalendars {
 	public static void main(String[] args) {
 		int a,b;
 		Scanner scanner = new Scanner(System.in);
-		String s1,s2;
 		System.out.println("두 수를 입력하시오.");
-		s1 = scanner.next();
-		s2 = scanner.next();
-		a = Integer.parseInt(s1);
-		b = Integer.parseInt(s2);
+		a = scanner.nextInt();
+		b = scanner.nextInt();
 		System.out.printf("두 수의 합은 %d입니다.", a + b);
 		scanner.close();
 	}
